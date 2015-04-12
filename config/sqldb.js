2ba1013d600991d258db2b05aa433345adc58dc1
Arjun Mantri <arjunmantri@users.noreply.github.com>
2015-04-12 03:57:50 -0700

    Update sqldb.js

--- config/sqldb.js
+++ config/sqldb.js
@@ -3,14 +3,14 @@ var mysqlcon;
 
 var connection = mysql.createConnection(
         {
-            "host":"demoapp.cnfiarybvhfe.us-west-1.rds.amazonaws.com",
-             "database":"sampledb",
-              "port":3306,
-                "username":"root",
-              "password":"rootroot",
-               "reconnect":true,
-                "data_source_provider":"rds",
-                 "type":"mysql"
+	"host":"sampledb.cy0fkl5xnx4r.us-west-1.rds.amazonaws.com",
+        "database":"sampledb",
+        "port":3306,
+        "username":"root",
+        "password":"12345678",
+        "reconnect":true,
+        "data_source_provider":"rds",
+        "type":"mysql"
         });
 
 
