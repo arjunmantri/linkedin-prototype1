febd1c2c00ec10656ebf09f97ee27aa6870f69fa
Arjun Mantri <arjunmantri@users.noreply.github.com>
2015-04-12 03:56:23 -0700

    Update mySqlConn.js

--- config/mySqlConn.js
+++ config/mySqlConn.js
@@ -1,20 +1,20 @@
 var mysql_pool = require('mysql');
 var pool  = mysql_pool.createPool({
-			"host":"demoapp.cnfiarybvhfe.us-west-1.rds.amazonaws.com",
-             "database":"sampledb",
-              "port":3306,
-               "username":"root",
-               "password":"rootroot",
-               "reconnect":true,
-                "data_source_provider":"rds",
-                 "type":"mysql",
+	"host":"sampledb.cy0fkl5xnx4r.us-west-1.rds.amazonaws.com",
+        "database":"sampledb",
+        "port":3306,
+        "username":"root",
+        "password":"12345678",
+        "reconnect":true,
+        "data_source_provider":"rds",
+        "type":"mysql",
 
 	// host     : 'localhost',
 	// user     : 'root',
 	// password : 'root',
 	// port     : '3306',
 	// database : 'Users',
-	"connectionLimit" : 10
+	//"connectionLimit" : 10
 });
 
 exports.pool = pool;
